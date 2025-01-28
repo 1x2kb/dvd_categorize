14732590de52865a87aa5175892d6769b0e7fbb7
1x2kb <1x2kb@users.noreply.github.com>
2025-01-27 20:17:24 -0600

    Added api and refactored code to use it. (#1)
    * Added api and refactored code to use it.
    
    * Fixed bug with csv_reader referencing the wrong csv file
    
    * Fixed test

--- categorizer/src/main.rs
+++ categorizer/src/main.rs
@@ -3,6 +3,7 @@ use dotenvy::dotenv;
 use iced::{Application, Settings};
 
 fn main() -> iced::Result {
+    pretty_env_logger::init();
     dotenv()
         .ok()
         .expect("Failed to run env reader");
--- database/src/lib.rs
+++ database/src/lib.rs
@@ -3,56 +3,103 @@ mod schema;
 
 use std::env;
 use std::error::Error;
+use std::fmt::Display;
 
 pub use diesel::prelude::*;
-pub use diesel::BelongingToDsl;
+use diesel_async::{AsyncConnection, AsyncPgConnection, RunQueryDsl};
+use log::debug;
 pub use models::*;
-use schema::{director, movie_genre};
+use schema::movie_genre;
 
-fn get_database_connection() -> Result<PgConnection, ConnectionError> {
-    let database_url =
-        env::var("DATABASE_URL").expect("No database information found, cannot connect");
-    PgConnection::establish(&database_url)
+#[cfg(feature = "testing")]
+pub trait Random {
+    fn random() -> Self;
 }
 
-pub fn get_movies() -> Result<Vec<FullMovie>, diesel::result::Error> {
-    let mut connection =
-        get_database_connection().unwrap_or_else(|_| panic!("Failed to establish a connection"));
+#[derive(Debug)]
+pub enum DatabaseError {
+    ConnectionError(ConnectionError),
+    DieselError(diesel::result::Error),
+}
+
+impl From<ConnectionError> for DatabaseError {
+    fn from(value: ConnectionError) -> Self {
+        DatabaseError::ConnectionError(value)
+    }
+}
+
+impl From<diesel::result::Error> for DatabaseError {
+    fn from(value: diesel::result::Error) -> Self {
+        DatabaseError::DieselError(value)
+    }
+}
+
+impl Error for DatabaseError {}
+
+impl Display for DatabaseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:#?}",
+            self
+        )
+    }
+}
+
+async fn get_database_connection() -> Result<AsyncPgConnection, DatabaseError> {
+    let database_url =
+        env::var("DATABASE_URL").expect("No database information found, cannot connect");
+    AsyncPgConnection::establish(&database_url)
+        .await
+        .map_err(DatabaseError::from)
+}
+
+pub async fn get_movies() -> Result<Vec<FullMovie>, DatabaseError> {
+    let mut connection = get_database_connection().await?;
 
     let movies = schema::movie::table
         .left_join(schema::director::table)
         .get_results::<(
             Movie,
             Option<Director>,
-        )>(&mut connection)?;
+        )>(&mut connection)
+        .await
+        .map_err(DatabaseError::from)?;
 
-    let full_movies = movies
-        .into_iter()
-        .map(
-            |(movie, director)| {
-                let actors = MovieActor::belonging_to(&movie)
-                    .inner_join(schema::actor::table)
-                    .select(schema::actor::all_columns)
-                    .load::<Actor>(&mut connection)
-                    .unwrap_or_else(|_| Vec::new());
+    let mut full_movies = Vec::new();
 
-                let genres = MovieGenre::belonging_to(&movie)
-                    .select(movie_genre::genre)
-                    .load::<String>(&mut connection)
-                    .unwrap_or_else(|_| Vec::new());
+    for (movie, director) in movies {
+        let actors = MovieActor::belonging_to(&movie)
+            .inner_join(schema::actor::table)
+            .select(schema::actor::all_columns)
+            .load::<Actor>(&mut connection)
+            .await
+            .unwrap_or_else(|_| Vec::new());
 
-                FullMovie::from((
-                    movie, director, actors, genres,
-                ))
-            },
-        )
-        .collect();
+        let genres = MovieGenre::belonging_to(&movie)
+            .select(movie_genre::genre)
+            .load::<String>(&mut connection)
+            .await
+            .unwrap_or_else(|_| Vec::new());
 
+        full_movies.push(
+            FullMovie::from((
+                movie, director, actors, genres,
+            )),
+        );
+    }
+
+    debug!(
+        "Found {} results",
+        full_movies.len()
+    );
     Ok(full_movies)
 }
 
-pub fn get_movie(id: i32) -> Result<FullMovie, Box<dyn Error>> {
-    let mut connection = get_database_connection()?;
+pub async fn get_movie(id: i32) -> Result<FullMovie, DatabaseError> {
+    let mut connection = get_database_connection()
+        .await
+        .map_err(DatabaseError::from)?;
 
     let (movie, director) = schema::movie::table
         .find(id)
@@ -60,17 +107,20 @@ pub fn get_movie(id: i32) -> Result<FullMovie, Box<dyn Error>> {
         .get_result::<(
             Movie,
             Option<Director>,
-        )>(&mut connection)?;
+        )>(&mut connection)
+        .await?;
 
     let actors = actors_for_movie(
         &movie,
         &mut connection,
-    );
+    )
+    .await;
 
     let genres = genres_for_movie(
         &movie,
         &mut connection,
-    );
+    )
+    .await;
 
     Ok(
         FullMovie::from((
@@ -79,8 +129,10 @@ pub fn get_movie(id: i32) -> Result<FullMovie, Box<dyn Error>> {
     )
 }
 
-pub fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, Box<dyn Error>> {
-    let mut conn = get_database_connection()?;
+pub async fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, DatabaseError> {
+    let mut conn = get_database_connection()
+        .await
+        .map_err(DatabaseError::from)?;
 
     // Director is optional, insert or select id for director name.
     let director_id: Option<i32> = match full_movie.director {
@@ -92,9 +144,11 @@ pub fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, Box<dyn Err
             let director_id: i32 = diesel::insert_into(schema::director::table)
                 .values(&new_director)
                 .on_conflict(schema::director::name)
-                .do_nothing()
+                .do_update()
+                .set(schema::director::id.eq(schema::director::id))
                 .returning(schema::director::id)
-                .get_result(&mut conn)?;
+                .get_result(&mut conn)
+                .await?;
 
             Some(director_id)
         }
@@ -110,7 +164,8 @@ pub fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, Box<dyn Err
     let movie_id = diesel::insert_into(schema::movie::table)
         .values(&new_movie)
         .returning(schema::movie::id)
-        .get_result::<i32>(&mut conn)?;
+        .get_result::<i32>(&mut conn)
+        .await?;
 
     let actors: Vec<NewActor> = full_movie
         .actors
@@ -121,9 +176,11 @@ pub fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, Box<dyn Err
     let actors = diesel::insert_into(schema::actor::table)
         .values(&actors)
         .on_conflict(schema::actor::name)
-        .do_nothing()
+        .do_update()
+        .set(schema::actor::id.eq(schema::actor::id))
         .returning(schema::actor::id)
-        .get_results::<i32>(&mut conn)?;
+        .get_results::<i32>(&mut conn)
+        .await?;
 
     let new_actor_movies: Vec<NewMovieActor> = actors
         .into_iter()
@@ -132,7 +189,8 @@ pub fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, Box<dyn Err
 
     let _ = diesel::insert_into(schema::movie_actor::table)
         .values(&new_actor_movies)
-        .execute(&mut conn)?;
+        .execute(&mut conn)
+        .await?;
 
     if !full_movie
         .genres
@@ -146,25 +204,28 @@ pub fn insert_full_movie(full_movie: FullMovie) -> Result<FullMovie, Box<dyn Err
 
         let _ = diesel::insert_into(schema::movie_genre::table)
             .values(&movie_genres)
-            .execute(&mut conn);
+            .execute(&mut conn)
+            .await;
     }
 
     // TODO: Return results of insert so lookup is unnecessary.
-    get_movie(movie_id)
+    get_movie(movie_id).await
 }
 
-pub fn actors_for_movie(movie: &Movie, connection: &mut PgConnection) -> Vec<Actor> {
+pub async fn actors_for_movie(movie: &Movie, connection: &mut AsyncPgConnection) -> Vec<Actor> {
     MovieActor::belonging_to(&movie)
         .inner_join(schema::actor::table)
         .select(schema::actor::all_columns)
         .load::<Actor>(connection)
+        .await
         .unwrap_or_else(|_| Vec::new())
 }
 
-pub fn genres_for_movie(movie: &Movie, connection: &mut PgConnection) -> Vec<String> {
+pub async fn genres_for_movie(movie: &Movie, connection: &mut AsyncPgConnection) -> Vec<String> {
     MovieGenre::belonging_to(&movie)
         .select(movie_genre::genre)
         .load::<String>(connection)
+        .await
         .unwrap_or_else(|_| Vec::new())
 }
 // pub fn insert_movie()
