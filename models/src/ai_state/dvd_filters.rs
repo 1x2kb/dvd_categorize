fe027503387df77b49ecba24658e19ba4445ab5e
1x2kb <1x2kb@users.noreply.github.com>
2025-02-08 18:29:08 -0600

    Added db lookup step prior to AI question (#5)

---
+++ models/src/ai_state/dvd_filters.rs
@@ -0,0 +1,16 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Default)]
+#[serde(rename_all = "snake_case")]
+pub struct DvdFilters {
+    pub genres: Option<Vec<String>>,
+
+    pub actors: Option<Vec<String>>,
+    pub directors: Option<Vec<String>>,
+
+    #[serde(rename = "movie_name")]
+    pub movie_names: Option<Vec<String>>,
+
+    #[serde(rename = "plot_keywords")]
+    pub plot_terms: Option<Vec<String>>,
+}
