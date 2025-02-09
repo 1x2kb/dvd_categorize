26e4a825422578295245a75aed1ee54f077cd48e
1x2kb <natemeyer@protonmail.com>
2025-02-08 18:27:53 -0600

    Added db lookup step prior to AI question

--- dvd_catalog_api/src/lib.rs
+++ dvd_catalog_api/src/lib.rs
@@ -2,7 +2,7 @@ use std::sync::Arc;
 
 use axum::{extract::Path, response, Json};
 use axum_macros::debug_handler;
-use database::{question::AiAction, FullMovie};
+use database::{dvd_filters::DvdFilters, movie_genre::movie_id, question::AiAction, FullMovie};
 use log::error;
 use tracing::instrument;
 
@@ -54,13 +54,34 @@ pub async fn chat(Json(action): Json<AiAction>) -> Json<AiAction> {
         action.model,
     );
 
+    let movie_ids = match ai_chat::find_related_keys(question.as_str()).await {
+        Ok(dvd_filters) => database::run_dvd_filters(dvd_filters)
+            .await
+            .ok(),
+        Err(e) => {
+            error!(
+                "{:#?}",
+                e
+            );
+            None
+        }
+    };
+
+    let full_movies = if let Some(movie_ids) = movie_ids {
+        database::get_movies_by_ids(movie_ids)
+            .await
+            .unwrap_or_else(|_| dvds)
+    } else {
+        dvds
+    }; // For now fall back to all dvds
+
     let result = ai_chat::ai_message(
         AiAction {
             uuid: uuid.to_string(),
             action: question,
             model: model.clone(),
         },
-        Arc::new(dvds),
+        Arc::new(full_movies),
     )
     .await;
 
