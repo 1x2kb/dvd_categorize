5afa3da5745ad761dc766d0a664d715f6b6e6b9a
1x2kb <1x2kb@users.noreply.github.com>
2025-02-23 08:56:01 -0600

    Refactors on AI structs and usage (#7)
    * Refactors on AI structs and usage
    
    * Cleanup
    
    * Clippy
    
    * Format

--- dvd_catalog_api/src/lib.rs
+++ dvd_catalog_api/src/lib.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
-use axum::{extract::Path, response, Json};
+use ai_chat::OllamaClient;
+use axum::{extract::Path, Json};
 use axum_macros::debug_handler;
-use database::{dvd_filters::DvdFilters, movie_genre::movie_id, question::AiAction, FullMovie};
+use database::{question::AiAction, FullMovie};
 use log::error;
+use ollama_rs::Ollama;
 use tracing::instrument;
 
 #[instrument]
@@ -57,7 +59,8 @@ pub async fn chat(Json(action): Json<AiAction>) -> Json<AiAction> {
     let movie_ids = match ai_chat::find_related_keys(question.as_str()).await {
         Ok(dvd_filters) => database::run_dvd_filters(dvd_filters)
             .await
-            .ok(),
+            .ok()
+            .filter(|movies| !movies.is_empty()),
         Err(e) => {
             error!(
                 "{:#?}",
@@ -70,18 +73,23 @@ pub async fn chat(Json(action): Json<AiAction>) -> Json<AiAction> {
     let full_movies = if let Some(movie_ids) = movie_ids {
         database::get_movies_by_ids(movie_ids)
             .await
-            .unwrap_or_else(|_| dvds)
+            .unwrap_or(dvds)
     } else {
         dvds
     }; // For now fall back to all dvds
 
     let result = ai_chat::ai_message(
-        AiAction {
-            uuid: uuid.to_string(),
-            action: question,
-            model: model.clone(),
-        },
         Arc::new(full_movies),
+        Arc::new(
+            OllamaClient {
+                ollama_client: Ollama::default(),
+                ai_action: AiAction {
+                    uuid: uuid.to_string(),
+                    action: question,
+                    model: model.clone(),
+                },
+            },
+        ),
     )
     .await;
 
