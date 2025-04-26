f7f24f31197ab8befc2a865e2cd0c3cf85472ecb
1x2kb <natemeyer@protonmail.com>
2025-04-26 14:50:56 -0500

    Fixed typo in table name.

--- database/migrations/2025-04-25-021841_create_vector_column/up.sql
+++ database/migrations/2025-04-25-021841_create_vector_column/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
-ALTER TABLE movies
+ALTER TABLE movie
 ALTER COLUMN embedding TYPE vector (768);
 
-CREATE INDEX movies_embedding_idx ON movies USING hnsw (embedding vector_cosine_ops)
+CREATE INDEX movies_embedding_idx ON movie USING hnsw (embedding vector_cosine_ops)
 WITH
     (m = 16, ef_construction = 64);
