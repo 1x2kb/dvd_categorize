e500a062bf355426545a380b27becbde01c2451a
1x2kb <natemeyer@protonmail.com>
2025-04-26 14:49:24 -0500

    Updated migration to a 768 column

--- database/migrations/2025-04-25-021841_create_vector_column/up.sql
+++ database/migrations/2025-04-25-021841_create_vector_column/up.sql
@@ -1,5 +1,7 @@
 -- Your SQL goes here
-ALTER TABLE movie
-ADD COLUMN embedding VECTOR (384);
+ALTER TABLE movies
+ALTER COLUMN embedding TYPE vector (768);
 
-CREATE INDEX movies_embedding_idx ON movie USING hnsw (embedding vector_cosine_ops);
+CREATE INDEX movies_embedding_idx ON movies USING hnsw (embedding vector_cosine_ops)
+WITH
+    (m = 16, ef_construction = 64);
