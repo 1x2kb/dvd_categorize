96041cc1b7f64e8cacc0aa8bc7af09404d376f6d
1x2kb <1x2kb@users.noreply.github.com>
2025-06-25 11:13:24 -0500

    Add dioxsus (#9)
    * Updated code with dioxus front-end. Also included vectorization for RAG data lookup for user search.

---
+++ dvd_categorizer_web/src/views/ai_chat.rs
@@ -0,0 +1,81 @@
+use dioxus::html::button;
+use dioxus::prelude::*;
+use log::{error, info};
+use models::{AiResponse, Role, RoledMessage};
+use serde::{Deserialize, Serialize};
+
+use crate::app_data::AppData;
+
+#[derive(Debug, Clone, Serialize, Deserialize, Eq, PartialEq)]
+pub struct AiAction {
+    pub uuid: String,
+    pub action: String,
+    pub model: Option<String>,
+}
+
+#[component]
+pub fn AiChat() -> Element {
+    let mut app_data = consume_context::<Signal<AppData>>();
+    let mut input_value = use_signal(|| String::new());
+
+    rsx! {
+
+        div {
+                class: "chat-history",
+                {app_data.read().ai_chat.read().as_ref().map(|messages| {
+                    rsx! {
+                        {app_data.read().ai_chat.read().as_ref().map(|messages| {
+                            rsx! {
+                                for (index, message) in messages.iter().enumerate() {
+                                    div {
+                                        key: "{index}-{message.role}",
+                                        // Base classes always applied
+                                        class: "message white-space-pre",
+                                        // Conditional class
+                                        class: if message.role == Role::Ai { "right-align" },
+                                        h3 { "{message.role}" }
+                                        p { "{message.message}" }
+                                    }
+                                }
+                            }
+                        })}
+                    }
+                    })}
+            }
+
+        input {
+            value: "{input_value}",
+            oninput: move |event| input_value.set(event.value()),
+        }
+        button { onclick: move |_| {
+            spawn(async move {
+                let server_host = std::env::var("server_host").unwrap_or("127.0.0.1".to_string());
+                let server_port = std::env::var("server_port").unwrap_or("3000".to_string());
+                let ai_action = AiAction { uuid: String::new(), action: input_value.read().clone(), model: Some("llama3.2".to_string()) };
+
+                app_data.write().ai_chat.with_mut(|chat| {
+                    if let Some(chat) = chat {
+                        chat.push(RoledMessage { message: ai_action.action.to_string(), role: Role::User });
+                    } else {
+                        *chat = Some(vec![RoledMessage { message: ai_action.action.to_string(), role: Role::User }]);
+                    }
+                });
+
+                let client = reqwest::Client::new();
+                let result = client.post(format!("http://{server_host}:{server_port}/ai/chat")).json(&ai_action).send().await;
+
+                match result {
+                    Ok(response) => {
+                        let roled_message = response.json::<AiResponse>().await.map(|ai_response| RoledMessage { message: ai_response.message, role: Role::Ai }).unwrap_or_else(|e| {error!("failed to parse the response {e}"); RoledMessage { message: "Failed to generate a response".to_string(), role: Role::Ai }});
+                        app_data.write().ai_chat.with_mut(|chat| {
+                            if let Some(chat) = chat {
+                                chat.push(roled_message);
+                            }
+                        });
+                    },
+                    Err(_) => {}, // Do nothing for now
+                }
+            });
+        }, "Submit" }
+    }
+}
